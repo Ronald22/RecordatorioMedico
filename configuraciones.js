40f28e93bf2d484ef2f080f9a252cdcccfa12ba1
Ronald22 <ronald22castro@gmail.com>
2017-10-03 23:47:00 -0500

    version 0.7

--- configuraciones.js
+++ configuraciones.js
@@ -1,7 +1,7 @@
 exports.dbConfig = {
 	user: "sa",
 	password: "ronald22",
-	server: "192.168.0.13\\RONALDCP",
+	server: "RONALD\\RONALDCP",
 	database: "RecordatorioMedico",
 	port: 1433,
 	options: {
