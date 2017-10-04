be46eea66e9d3a4453ea0589418c7f28b91a9f07
Ronald22 <ronald22castro@gmail.com>
2017-10-04 14:01:13 -0500

    2.6

--- configuraciones.js
+++ configuraciones.js
@@ -2,6 +2,7 @@ exports.dbConfig = {
     userName: 'sa', // update me
     password: 'ronald22', // update me
     server: 'localhost', // update me
+    port:49172,
     options: {
         database: 'RecordatorioMedico', //update me
         encrypt: true
